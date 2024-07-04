fcb894290b5131ab2bee989f8d1a02beded84ed2
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-07-05 03:45:23 +0530

    update main in backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,17 @@
-use candle_core::{DType, Device, Tensor};
+use candle_core::Device;
 use hf_hub::api::tokio::Api;
-
-use candle_nn::{Linear, Module};
 
 #[tokio::main]
 async fn main() {
+    let device = Device::Cpu;
     let api = Api::new().unwrap();
-    let repo = api.model("bert-base-uncased".to_string());
+    let repo = api.model("BAAI/bge-small-en".to_string());
 
     let weights_filename = repo.get("model.safetensors").await.unwrap();
 
-    let weights = candle_core::safetensors::load(weights_filename, &Device::Cpu).unwrap();
+    let weights = candle_core::safetensors::load(weights_filename, &device).unwrap();
 
-    let weight = weights
-        .get("bert.encoder.layer.0.attention.self.query.weight")
-        .unwrap();
-    let bias = weights
-        .get("bert.encoder.layer.0.attention.self.query.bias")
-        .unwrap();
+    println!("{:#?}", weights);
 
-    let linear = Linear::new(weight.clone(), Some(bias.clone()));
 
-    let input_ids = Tensor::zeros((3, 768), DType::F32, &Device::Cpu).unwrap();
-    let output = linear.forward(&input_ids).unwrap();
-    println!("{output}");
 }
